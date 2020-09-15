f7f9c20e23a41e80a8fe82e6270ee5abf4a9bbaa
akbarramadhannnn <akbarramadhan452@gmail.com>
2020-09-15 13:45:35 +0700

    update

--- backend/routes/kriteria.js
+++ backend/routes/kriteria.js
@@ -1,10 +1,11 @@
 const express      = require('express')
 const router       = express.Router()
 
-const { tambahKriteria, getKriteria, getKriteriaById } = require('../controllers/kriteria')
+const { tambahKriteria, getKriteria, getKriteriaById, perhitunganBobot } = require('../controllers/kriteria')
 
 router.get('/kriteria', getKriteria)
 router.get('/kriteria/:idKriteria', getKriteriaById)
 router.post('/kriteria/tambah', tambahKriteria)
+router.post('/kriteria/perhitunganbobot', perhitunganBobot)
 
 module.exports = router
