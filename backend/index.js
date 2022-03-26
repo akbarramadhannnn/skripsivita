a0874ee767152083f6e75621d75b7f442eed8243
akbarramadhannnn <akbarramadhan452@gmail.com>
2022-03-26 19:39:47 +0700

    update data db

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,7 @@ mongoose
     // .connect('mongodb://localhost:27017/skripsivita', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(() => {
     console.log("Database terhubung.. nama database 'vitaskripsi' ");
